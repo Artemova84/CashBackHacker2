3a3039c36b6f12283d03ae53e13bcfad7e27a598
Artemova <artemovaok84@mail.ru>
2023-06-17 20:28:54 +0300

    Commit with TestNG

---
+++ src/test/java/ru/netology/cashBackHacker/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.cashBackHacker;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void CashbackLessThousand(){
+        CashbackHackService cashback = new CashbackHackService();
+        int actual = cashback.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void CashbackMoreThousand(){
+        CashbackHackService cashback = new CashbackHackService();
+        int actual = cashback.remain(1100);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void CashbackTwoThousand(){
+        CashbackHackService cashback = new CashbackHackService();
+        int actual = cashback.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+}
