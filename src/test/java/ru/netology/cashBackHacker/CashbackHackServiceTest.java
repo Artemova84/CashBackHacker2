2a340355cb43ee1f1c87c9624458c1593e37536f
Artemova <artemovaok84@mail.ru>
2023-06-18 09:16:42 +0300

    reformat code

--- src/test/java/ru/netology/cashBackHacker/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/cashBackHacker/CashbackHackServiceTest.java
@@ -7,21 +7,23 @@ import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
 
     @Test
-    public void CashbackLessThousand(){
+    public void CashbackLessThousand() {
         CashbackHackService cashback = new CashbackHackService();
         int expected = 100;
         int actual = cashback.remain(900);
         assertEquals(expected, actual);
     }
+
     @Test
-    public void CashbackMoreThousand(){
+    public void CashbackMoreThousand() {
         CashbackHackService cashback = new CashbackHackService();
         int expected = 900;
         int actual = cashback.remain(1100);
         assertEquals(expected, actual);
     }
+
     @Test
-    public void CashbackTwoThousand(){
+    public void CashbackTwoThousand() {
         CashbackHackService cashback = new CashbackHackService();
         int expected = 0;
         int actual = cashback.remain(1000);
